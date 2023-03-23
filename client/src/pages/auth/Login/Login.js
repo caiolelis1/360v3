379bbd444ad836f1a87fc3fa2a1f1d9939800d82
marcos lott <lott.marcos@gmail.com>
2023-03-23 15:21:09 -0300

    fixing loginpage

--- client/src/pages/auth/Login/Login.js
+++ client/src/pages/auth/Login/Login.js
@@ -32,54 +32,52 @@ const Login = () => {
   };
 
   return (
-    <section className="DefaultPage">
-      <section className="AuthPageBox">
-        <section className="AuthPageTitleSection">
-          <img src={logo} alt="Logo TESLA - UFMG" className="AuthPageIMG" />
-          <h1 className="AuthPageTitle">Login</h1>
-        </section>
-        <Formik
-          initialValues={{}}
-          onSubmit={handleLogin}
-          validationSchema={validationLogin}
-        >
-          <Form>
-            <div className="FormikInputDiv">
-              <Field
-                name="username"
-                className="FormikInputField"
-                placeholder="Nome de usuário"
-              />
-              <div className="FormikErrorMessage">
-                <ErrorMessage name="username" />
-              </div>
+    <section className="AuthPageBox">
+      <section className="AuthPageTitleSection">
+        <img src={logo} alt="Logo TESLA - UFMG" className="AuthPageIMG" />
+        <h1 className="AuthPageTitle">Login</h1>
+      </section>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleLogin}
+        validationSchema={validationLogin}
+      >
+        <Form>
+          <div className="FormikInputDiv">
+            <Field
+              name="username"
+              className="FormikInputField"
+              placeholder="Nome de usuário"
+            />
+            <div className="FormikErrorMessage">
+              <ErrorMessage name="username" />
             </div>
-            <div className="FormikInputDiv">
-              <Field
-                type="password"
-                name="password"
-                className="FormikInputField"
-                placeholder="Senha"
-              />
-              <div className="FormikErrorMessage">
-                <ErrorMessage name="password" />
-              </div>
+          </div>
+          <div className="FormikInputDiv">
+            <Field
+              type="password"
+              name="password"
+              className="FormikInputField"
+              placeholder="Senha"
+            />
+            <div className="FormikErrorMessage">
+              <ErrorMessage name="password" />
             </div>
-            {err && err}
-            <div className="SubmitButtonDiv">
-              <button className="SubmitButton" type="submit">
-                Login
-              </button>
-            </div>
-          </Form>
-        </Formik>
-        <section className="AuthPageLinksSection">
-          <p className="AuthPageLinksText">
-            Não possui uma conta?
-            <br />
-            <a href="/cadastro">Crie uma conta.</a>
-          </p>
-        </section>
+          </div>
+          {err && err}
+          <div className="SubmitButtonDiv">
+            <button className="SubmitButton" type="submit">
+              Login
+            </button>
+          </div>
+        </Form>
+      </Formik>
+      <section className="AuthPageLinksSection">
+        <p className="AuthPageLinksText">
+          Não possui uma conta?
+          <br />
+          <a href="/cadastro">Crie uma conta.</a>
+        </p>
       </section>
     </section>
   );
